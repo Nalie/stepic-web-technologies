8da63a3c838ab6291137c8b7d8c198fe4f90195f
nsdjapparova <nyapparova@gmail.com>
2016-03-05 00:33:48 +0300

    error

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sudo mkdir /home/box/web/public/img
 sudo mkdir /home/box/web/public/css
 sudo mkdir /home/box/web/uploads
 
-sudo -R chown www-data /home/box/web
+sudo chown -R www-data /home/box/web
 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
