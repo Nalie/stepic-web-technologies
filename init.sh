0d8e4f881d16d35b0720eba2fda3242d76aef6a4
nsdjapparova <nyapparova@gmail.com>
2016-03-05 00:22:55 +0300

    error-log + chown

--- init.sh
+++ init.sh
@@ -3,5 +3,7 @@ sudo mkdir /home/box/web/public/img
 sudo mkdir /home/box/web/public/css
 sudo mkdir /home/box/web/uploads
 
+sudo -R chown www-data /home/box/web
+
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
