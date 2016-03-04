90f500d12dcaa66573795ba04eaab5c930c060e4
nsdjapparova <nyapparova@gmail.com>
2016-03-05 00:04:17 +0300

    test.js added

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 sudo mkdir /home/box/web/public
 sudo mkdir /home/box/web/public/img
 sudo mkdir /home/box/web/public/css
-sudo mkdir /home/box/web/public/js
 sudo mkdir /home/box/web/uploads
 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
