611092b462b4bce3a5ae12acedbbf1a89af31f82
nsdjapparova <nyapparova@gmail.com>
2016-03-10 23:29:07 +0300

    no message

--- init.sh
+++ init.sh
@@ -11,3 +11,5 @@ sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
 cd /home/box/web
 sudo gunicorn -c /etc/gunicorn.d/test hello:app &
+cd ask
+sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application &
