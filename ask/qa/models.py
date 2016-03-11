b38a914a95ff249a8c2274db2fd91327edc9bf33
nsdjapparova <nyapparova@gmail.com>
2016-03-12 00:22:43 +0300

    rating not null

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(null=False, default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='questions_set')
     likes = models.ManyToManyField(User, related_name='likes_set')
     def __unicode__(self):
