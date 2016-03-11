bfd009fd6297b5396386b29f71d9eca384044ada
nsdjapparova <nyapparova@gmail.com>
2016-03-11 13:46:31 +0300

    db settings

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -56,9 +56,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'ask',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
