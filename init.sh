19e43399ac9a9073e5d110e5a7f4e369505f9391
nsdjapparova <nyapparova@gmail.com>
2016-03-12 23:27:59 +0300

    Step 2.6 ends

--- init.sh
+++ init.sh
@@ -6,19 +6,18 @@ sudo mkdir /home/box/web/uploads
 
 sudo chown -R www-data /home/box/web
 
-#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-#sudo /etc/init.d/nginx restart
-sudo /etc/init.d/nginx stop
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
 cd /home/box/web
-#sudo gunicorn -c /etc/gunicorn.d/test hello:app &
+sudo gunicorn -c /etc/gunicorn.d/test hello:app &
 cd ask
-#sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application &
+sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application &
 
 mysql -uroot -e "create database ask"
 mysqladmin -u root password pass
 ./manage.py syncdb
 
-sudo python /home/box/web/ask/manage.py runserver 0.0.0.0:80
+#sudo python /home/box/web/ask/manage.py runserver 0.0.0.0:80
