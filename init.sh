b1ef9b6df7efa77a009bf8f75cdc431035535265
nsdjapparova <nyapparova@gmail.com>
2016-03-16 18:57:30 +0300

    no message

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ sudo chown -R www-data /home/box/web
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-# sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
 cd /home/box/web
 sudo gunicorn -c /etc/gunicorn.d/test hello:app &
 cd ask
