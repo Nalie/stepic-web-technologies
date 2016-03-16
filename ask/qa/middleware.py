eabf6ddeff90f10b0120e1d589587d843e3841f7
nsdjapparova <nyapparova@gmail.com>
2016-03-16 15:15:10 +0300

    no message

--- ask/qa/middleware.py
+++ ask/qa/middleware.py
@@ -7,7 +7,7 @@ class CheckSessionMiddleware(object):
 
     def process_request(self, request):
         try:
-            sessid = request.COOKIES.get('sessionId')
+            sessid = request.COOKIES.get('session')
             if sessid is not None:
                 session = Session.objects.get(
                     key=sessid,
